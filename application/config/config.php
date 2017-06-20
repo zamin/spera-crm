15adc320ab13ad208cf9678baa0a066137d07e41
AbydArs <abidr.w@gmail.com>
2017-06-21 01:39:55 +0500

    spera crm init

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://spera.localhost:2200/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
